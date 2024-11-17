8c19d884446932c19f2c5262513bc12ad9b470e0
Eric Smith <eric.smith@aurizn.co>
2024-11-17 20:39:11 +1030

    Updated: Sun Nov 17 20:39:11 2024

--- src/nerdraw.ts
+++ src/nerdraw.ts
@@ -1,22 +1,31 @@
 import * as fs from 'fs';
 import yaml from 'js-yaml';
 
-const diagram = process.argv[2]
-
 function usage() {
     console.log("Usage: nerdraw <file>");
     process.exit(0);
+}
+
+const shape_map = {
+    circle: {
+        element: "circle",
+        cx: [ "cx", 0 ],
+        cy: [ "cy", 0 ],
+        r: [ "r", 0 ]
+    }
 }
 
 if (process.argv.length < 3) {
     usage();
 }
 
-if (!fs.existsSync(diagram)) {
-    console.error(`${diagram} not found; aborting...`);
+const nd_input = process.argv[2]
+
+if (!fs.existsSync(nd_input)) {
+    console.error(`${nd_input} not found; aborting...`);
     process.exit(1);
 }
 
-const parsed = yaml.load(fs.readFileSync(diagram, { encoding: 'utf8' }))
+const nd_tree = yaml.load(fs.readFileSync(nd_input, { encoding: 'utf8' }))
 
-console.log(JSON.stringify(parsed))
+console.log(JSON.stringify(nd_tree))
